df5a6f4b35bfd29b69943ccb295455559876bd51
Minh Tri Nguyen <54835452+minhtringuyennn@users.noreply.github.com>
2021-02-24 17:21:37 +0700

    A fully working code - give it a go XD
    Just simply do as it should be

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys, datetime, subprocess
+	
+print("System started!")
+
+sys_max_hour = 1 #set maximum hours
+sys_start_time = datetime.datetime.now()
+delta = datetime.timedelta(hours = sys_maxhour)
+sys_shutdown_time = sys_start_time + delta
+
+def shutdown():
+	subprocess.call(["shutdown", "/s", "/f", "/t", "120",
+    "/c", "Còn 2 phút trước khi tắt máy. Sao lưu dữ liệu!"])
+	sys.exit()
+
+while True:
+	cur_time = datetime.datetime.now()
+	if(cur_time >= sys_shutdown_time):
+		shutdown()
